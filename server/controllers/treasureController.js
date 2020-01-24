10a06e0ff87054235078869362e96584e266213b
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-01-23 18:47:30 -0700

    Complete

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -14,5 +14,10 @@ module.exports = {
     const { id } = req.session.user;
     const userTreasure = await req.app.get('db').add_user_treasure([treasureURL, id]);
     return res.status(200).send(userTreasure);
+  },
+
+  getAllTreasure: async (req, res) => {
+    const allTreasure = await req.app.get('db').get_all_treasure();
+    return res.status(200).send(allTreasure);
   }
 }
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,5 +5,13 @@ module.exports = {
     } else {
       next();
     }
+  },
+
+  adminsOnly: (req, res, next) => {
+    if (!req.session.user.isAdmin) {
+      res.status(403).send('You are not an admin.');
+    }
+
+    next();
   }
 }
